2ef37c606dee1d483b2dc669993669e939107613
Shahzeb Ihsan <shahzeb@gmail.com>
2015-06-17 13:32:11 +0800

    Complete functionality for get_photo_urls_from_flickr_set.py

--- get_photo_urls_from_flickr_set.py
+++ get_photo_urls_from_flickr_set.py
@@ -1,6 +1,8 @@
-import sys, urllib2
+import os, sys, urllib2
 import flickrapi
 from HTMLParser import HTMLParser
+
+jssor_div_output = None
 
 api_key = 'ea5bf9b1668161e323060ad2d91accb1'
 api_secret = '17b2e7c7b5475b8b'
@@ -8,12 +10,52 @@ api_secret = '17b2e7c7b5475b8b'
 set_id = '72157622960195643'
 
 photo_ids = []
-page_large_1600 = []
+page_large = []
 page_thumbnails = []
-url_large_1600 = []
+url_large = []
 url_thumbnails = []
 
-print '\r\n'
+def print_progress_bar(n, total, step = 1, msg = ''):
+    str = '' + msg
+
+    percentage = (100 * n) / total
+
+    if n == total - 1:
+        percentage = 100
+
+    for p in range(percentage):
+        if p == 0:
+            str += '[%(n)3d%%] ' % {'n': percentage}
+
+        if (p % step) == 0:
+            str += '='
+
+    sys.stdout.write('\r' + str)
+    sys.stdout.flush()
+
+class FlickrDwnldPgParser(HTMLParser):
+    div_found = False
+    url = ''
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            if 'allsizes-photo' in attrs[0][1]:
+                self.div_found = True
+
+        if tag == "img" and self.div_found:
+            self.url = attrs[0][1]
+            self.div_found = False
+
+    def get_url(self):
+        return self.url
+
+print '\r\n----------------------------------------------------------------'
+if len(sys.argv) > 1:
+    jssor_div_output = open(os.path.join('', sys.argv[1]), 'wr')
+else:
+    sys.stderr.write('Error! No output file specified...')
+    sys.stderr.flush()
+    sys.exit(-1)
+
 print 'Initializing Flickr API ....... ',
 sys.stdout.flush()
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='etree')
@@ -32,21 +74,11 @@ print 'done!'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
 for n in range(len(photo_ids)):
-    str = 'Retrieving photo URLs: '
-    percentage = (100 * n) / len(photo_ids)
-
-    if n == len(photo_ids) - 1:
-        percentage = 100
-
-    for p in range(percentage):
-        if p == 0:
-            str += '[%(n)3d%%] ' % {'n': percentage}
-
-        if (p % 2) == 0:
-            str += '='
-
-    sys.stdout.write('\r' + str)
-    sys.stdout.flush()
+    print_progress_bar(
+                n,
+                len(photo_ids),
+                2,
+                'Retrieving URLs for image pages: ')
 
     sizes = flickr.photos.getSizes(photo_id = photo_ids[n])
 
@@ -54,10 +86,50 @@ for n in range(len(photo_ids)):
         label = sizes['sizes']['size'][m]['label']
         url = sizes['sizes']['size'][m]['url']
 
-        if label == 'Large 1600':
-            page_large_1600.append(url)
+        if (label == 'Large'):
+            page_large.append(url)
 
         if label == 'Thumbnail':
             page_thumbnails.append(url)
 
 print ' ...... done!'
+
+for n in range(len(page_large)):
+    print_progress_bar(
+                n,
+                len(page_large),
+                2,
+                'Retrieving image URLs (large): ')
+
+    response = urllib2.urlopen(page_large[n])
+    html = response.read()
+    parser = FlickrDwnldPgParser()
+    parser.feed(html)
+    url_large.append(parser.get_url())
+print 'done!'
+
+for n in range(len(page_thumbnails)):
+    print_progress_bar(
+                n,
+                len(page_thumbnails),
+                2,
+                'Retrieving image URLs (thumbnails): ')
+
+    response = urllib2.urlopen(page_thumbnails[n])
+    html = response.read()
+    parser = FlickrDwnldPgParser()
+    parser.feed(html)
+    url_thumbnails.append(parser.get_url())
+print 'done!'
+
+for n in range(len(url_large)):
+    jssor_div_output.write('    <div>\r\n')
+    jssor_div_output.write('        <img u=\"image\" src=\"' + url_large[n] + '\" />\r\n')
+    jssor_div_output.write('        <div u=\"thumb\">\r\n')
+    jssor_div_output.write('            <div style=\"width: 100%; height: 100%; background-image: url(' + url_thumbnails[n] + '); background-position: center center; background-repeat: no-repeat; \">\r\n')
+    jssor_div_output.write('            </div>\r\n')
+    jssor_div_output.write('        </div>\r\n')
+    jssor_div_output.write('    </div>\r\n')
+
+jssor_div_output.close()
+print '----------------------------------------------------------------\r\n'
