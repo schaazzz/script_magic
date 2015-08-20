fdccc1b124d227c7c92e8de23bda90e7b790c87d
Shahzeb Ihsan <shahzeb@gmail.com>
2015-08-20 13:01:38 +0800

    Updated reset_settings.sh to use `sudo` infront of each command

--- reset_settings.sh
+++ reset_settings.sh
@@ -5,18 +5,19 @@ set -i
 
 if [ $option = "network" ]; then
     echo Resetting the network...
-    systemctl  restart  systemd-networkd
-    resolvconf  -u
+    sudo systemctl restart systemd-networkd
+    sudo resolvconf -u
     echo Done!
 elif [ $option = "desktop" ]; then
     echo Resetting the LXQT desktop...
-    pcmanfm-qt  --desktop-off  --profile  lxqt
-    pcmanfm-qt  --desktop  --profile  lxqt &
+    sudo pcmanfm-qt --desktop-off --profile lxqt
+    sleep 3 
+    sudo pcmanfm-qt --desktop --profile lxqt &
     echo Done!
 elif [ $option = "clock" ]; then
     echo Resetting the clock...
-    ntpd -qg
-    hwclock --systohc
+    sudo ntpd -qg
+    sudo hwclock --systohc
     echo Done!
 else
     echo Unknown option: $option >&2
